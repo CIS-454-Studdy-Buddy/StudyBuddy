2756852b0d61c68922725ba9cbf2173934639f2a
Aaron Alakkadan <aalakkad@syr.edu>
2023-02-14 21:22:36 -0500

    config file

---
+++ instance/config.py
@@ -0,0 +1,10 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    SECRET_KEY = 'thisisasecretkey'
+    MAIL_SERVER ='smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USERNAME = 'su.study.buddy@gmail.com'
+    MAIL_PASSWORD = 'mvzuqlkwoedrkcsi'
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    
