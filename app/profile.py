54761c2233ed27328583ce3422650c089f7b0455
Aaron Alakkadan <aalakkad@syr.edu>
2023-02-24 14:27:10 -0500

    profileupdates and proficency test backend

--- app/profile.py
+++ app/profile.py
@@ -13,6 +13,8 @@ class profileForm(FlaskForm):
     saveButton = SubmitField("Save")
     phoneNumber = StringField(render_kw={"placeholder": "Phone Number"})
     aboutMe = StringField(render_kw={"placeholder": "About Me"})
+    removePhoneButton = SubmitField("remove my current phone number")
+    removeAboutMeButton = SubmitField("delete my current about me description")
 
 
 bp = Blueprint('profile', __name__, url_prefix='/')
@@ -32,19 +34,19 @@ def profile():
             # if phone number and about me are not changed, do not update user's fields
             if ((form.phoneNumber.data == "") and (form.aboutMe.data == "")):
                 msg = "Phone number and about me description not changed"
-                return redirect(url_for('dashboard.dashboard'))
+                return redirect(url_for('profile.profile'))
             # phone number is not changed and aboutMe is changed, update the user's about me description
             elif ((form.phoneNumber.data == "") and (form.aboutMe.data != "")):
                 user.about_me = form.aboutMe.data
                 db.session.add(user)
                 db.session.commit()
-                return redirect(url_for('dashboard.dashboard'))
+                return redirect(url_for('profile.profile'))
             # about me is not changed and phone number is changed, update the user's phone number
             elif ((form.phoneNumber.data != "") and (form.aboutMe.data == "")):
                 user.phone_number = form.phoneNumber.data
                 db.session.add(user)
                 db.session.commit()
-                return redirect(url_for('dashboard.dashboard'))
+                return redirect(url_for('profile.profile'))
 
             # phone number and about me fields are changed, so update both field for the user
             elif (((len(form.phoneNumber.data) <= 15) and form.phoneNumber.data.isnumeric()) and (len(form.aboutMe.data) <= 50)):
@@ -53,7 +55,7 @@ def profile():
                 user.about_me = form.aboutMe.data
                 db.session.add(user)
                 db.session.commit()
-                return redirect(url_for('dashboard.dashboard'))
+                return redirect(url_for('profile.profile'))
 
             # if phone number is too long or is invalid, send message to user
             elif ((len(form.phoneNumber.data) > 15) or (not form.phoneNumber.data.isnumeric())):
@@ -70,5 +72,20 @@ def profile():
 
         elif form.data['logoutButton']:
             return redirect(url_for('auth.login'))
+        # user clicks remove phone number button, delete current user's phone number from database
+        elif form.data['removePhoneButton']:
+            user.phone_number = ""
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('profile.profile'))
+
+        # user clicks remove about me description button, delete current user's about 
+        # me description from database
+        elif form.data['removeAboutMeButton']:
+            print("Reset about me description")
+            user.about_me = ""
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('profile.profile'))
 
     return render_template('profile.html', form=form, msg = msg)
--- app/utils.py
+++ app/utils.py
@@ -6,4 +6,23 @@ def create_db():
 
     with create_app().app_context():
         db.create_all()
+    
+def seed_data():
+   from app.extensions import db, bcrypt
+   from app import create_app
+   from app.models.user import User
+   from app.models.course import Course
+
+   with create_app().app_context():
+        hashed_password = bcrypt.generate_password_hash("123456")
+        new_user = User(first_name="Aaron", last_name="Alakkadan",  
+                            username="aalakkad@syr.edu", password=hashed_password,
+                            token="1234", is_verified=True)
+        db.session.add(new_user)
+
+        new_course = Course(name="Software Implementation", family="CIS",  
+                            number="454")
+        db.session.add(new_course)
+        db.session.commit() 
+
         
