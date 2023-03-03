27e7efde0550caffd556ba3f738d0d5cbd2c1d29
mattfaiola <65583930+mattfaiola@users.noreply.github.com>
2023-03-03 14:32:19 -0500

    adds random courses to user account for testing

--- app/utils.py
+++ app/utils.py
@@ -6,7 +6,8 @@ from app.models.course import Course, Subject
 from app.extensions import db
 import os
 import csv
-
+import random
+from statistics import mean
 
 def import_courses(file_path):
     with open(file_path, 'r') as csv_file:
@@ -61,8 +62,32 @@ def seed_data():
         new_user3 = User(first_name="Talal", last_name="Hakki",  
                             username="thakki@syr.edu", password=hashed_password,
                             token="4567", is_verified=True, phone_number="3151231234", about_me="I am a student at Syracuse University")
+        new_user4 = User(first_name="John", last_name="Fuhrman",  
+                            username="jnfuhrma@syr.edu", password=hashed_password,
+                            token="23498", is_verified=True, phone_number="31519463274", about_me="I am a student at Syracuse University")
+        new_user5 = User(first_name="Alec", last_name="Marcus",  
+                            username="almarcus@syr.edu", password=hashed_password,
+                            token="45456", is_verified=True, phone_number="3151633434", about_me="I am a student at Syracuse University")
         db.session.add(new_user)
         db.session.add(new_user2)
         db.session.add(new_user3)
+        db.session.add(new_user4)
+        db.session.add(new_user5)
+        db.session.commit()
+        users = [new_user, new_user2, new_user3, new_user4, new_user5]
+        courses = [44, 500, 411, 28, 3400]
+        for user in users:
+            for i in range(0, 5):
+                si = StudyInterest()
+                si.user_id = user.id
+                si.course_id = courses[i]
+                pro_ans1 = random.randint(1, 5)
+                si.pro_ans1 = pro_ans1
+                pro_ans2 = random.randint(1, 5)
+                si.pro_ans2 = pro_ans2
+                pro_ans3 = random.randint(1, 5)
+                si.pro_ans1 = pro_ans3
+                si.pro_score = round(mean([int(pro_ans1), int(pro_ans2), int(pro_ans3)]),2)
+                db.session.add(si)
         db.session.commit()
         
