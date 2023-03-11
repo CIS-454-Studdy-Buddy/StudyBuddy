6cb59b7712461d24c55292496cb00f78fc96f777
Aaron Alakkadan <aalakkad@syr.edu>
2023-03-11 13:16:29 -0500

    view ratings and some tests wip for displaying buddy star rating in dashboard

--- app/viewratings.py
+++ app/viewratings.py
@@ -2,13 +2,18 @@ from flask import Blueprint, render_template, url_for, redirect, request, sessio
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user 
 from app.auth import *
 from app.dashboard import *
+from app.models.user import User
+from app.models.buddyrating import BuddyRating
+from sqlalchemy import desc
 
 class viewRatingsForm(FlaskForm):
-    but = SubmitField("Button")
+    view_rating_but = SubmitField("Button")
 
 bp = Blueprint('viewratings', __name__, url_prefix='/')
 @bp.route('/viewratings', methods=['GET', 'POST'])
 @login_required
 def viewRatings():
     form = viewRatingsForm()
-    return render_template('viewratings.html', form=form)
+    user = User.query.filter_by(username=current_user.username).first()
+    buddy_rating = BuddyRating.query.filter_by(rating_receiver=user.id).order_by(desc(BuddyRating.rating_date)).all()
+    return render_template('viewratings.html', form=form, buddy_rating=buddy_rating)
