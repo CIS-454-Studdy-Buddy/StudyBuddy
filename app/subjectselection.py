50148cf8ce1d7e69a3eb74db4cb024921462b203
mattfaiola <65583930+mattfaiola@users.noreply.github.com>
2023-03-11 23:01:06 -0500

    login error fix, other html tweaks

--- app/subjectselection.py
+++ app/subjectselection.py
@@ -148,19 +148,3 @@ def codesortcourse(get_code):
         course['name'] = code.course_name
         course_array.append(course)
     return jsonify({'courselist': course_array})
-
-'''
-1. Create a route called subjectselections
-2. StudyInterest model filter by user code and return render with a template
-3. Create template and loop study interest objects and display
-'''
-@bp.route('/subjectselections', methods=['GET', 'POST'])
-@login_required
-def list_of_subjects_selected():
-    form = SubjectSelectionForm()
-    si = None
-    user = User.query.filter_by(username=current_user.username).first()
-    if user:
-        si = StudyInterest.query.filter_by(user_id=user.id).options(joinedload(StudyInterest.course)).all()
-        print(si)  
-    return render_template('subjectselections.html',form=form, si=si)
